e0a0f091aaa89f166da447c30d9b688d19da8246
flaviostutz <flaviostutz@gmail.com>
2016-05-26 12:47:11 -0300

    Initial creation

---
+++ software/esp8266-firmware/src/Main.ino
@@ -0,0 +1,39 @@
+#include <Homie.h>
+
+const int PIN_RELAY = 5;
+
+HomieNode lightNode("light", "switch");
+
+bool lightOnHandler(String value) {
+  if (value == "true") {
+    digitalWrite(PIN_RELAY, HIGH);
+    Homie.setNodeProperty(lightNode, "on", "true"); // Update the state of the light
+    Serial.println("Light is on");
+  } else if (value == "false") {
+    digitalWrite(PIN_RELAY, LOW);
+    Homie.setNodeProperty(lightNode, "on", "false");
+    Serial.println("Light is off");
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+void setup() {
+  pinMode(PIN_RELAY, OUTPUT);
+  digitalWrite(PIN_RELAY, LOW);
+
+  Homie.setFirmware("awesome-relay", "1.0.0");
+  lightNode.subscribe("on", lightOnHandler);
+  Homie.registerNode(lightNode);
+
+  Homie.enableBuiltInLedIndicator(false);
+  Homie.enableLogging(true);
+
+  Homie.setup();
+}
+
+void loop() {
+  Homie.loop();
+}
